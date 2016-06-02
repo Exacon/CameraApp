7b878d0f323e5e96c4f65bb3aadfa125ef0c6e52
tteig <tteig@DESKTOP-64FF8L9>
2016-06-02 14:55:13 +0200

    Add project files.

---
+++ CameraApp/CameraApp.Droid/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Provider;
+using Java.IO;
+
+namespace CameraApp.Droid
+{
+    [Activity(Label = "CameraApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        static readonly File file =new File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures),"tmp.jpg");
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+
+
+            var app = Xamarin.Forms.Application.Current as App;
+            if (app != null)
+                app.TakeAPicture += () =>
+                {
+                    var intent = new Intent(MediaStore.ActionImageCapture);
+                    intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(file));
+                    StartActivityForResult(intent,0);
+                    
+                };
+            app.Update(file.Path);
+
+            var o = Xamarin.Forms.Application.Current as App;
+            if (o != null)
+                o.ShowImage((file.Path));
+        }
+    }
+}
+
---
+++ CameraApp/CameraApp/App.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CameraApp
+{
+    public class App : Application
+    {
+        readonly Image image = new Image();
+
+
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Button
+                        {
+                            Text = "Ta et bilde",
+                            Command = new Command(o => TakeAPicture()),
+                        },
+                        image,
+                    },
+                },
+            };
+        }
+
+        public event Action TakeAPicture = () => { };
+
+        public void ShowImage(string path)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                image.Source = ImageSource.FromFile(path);
+            });
+
+        }
+
+        public void Update(string path)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                image.Source = ImageSource.FromFile(path);
+            });
+        }
+
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
